46f8aef5d52d56c62858760cf6393b5cc47ccb30
Aldo Herrera <aldonvacriates@gmail.com>
2025-10-07 12:44:50 -0600

    last push

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import OpengraphImage from "@/components/opengraph-image";
 
-export const runtime = "edge";
+export const runtime = "nodejs"; // forces Node runtime
+export const dynamic = "force-static"; // guarantees SSG
 
 export default async function Image() {
   return await OpengraphImage();
